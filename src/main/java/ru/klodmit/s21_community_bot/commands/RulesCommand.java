dae8f95e4c4d60ebddf67de1fad8cf5820d76b6f
klodmit <Dimaklochihin@yandex.ru>
2024-11-30 23:38:04 +0300

    changed RULES_MESSAGE

--- src/main/java/ru/klodmit/s21_community_bot/commands/RulesCommand.java
+++ src/main/java/ru/klodmit/s21_community_bot/commands/RulesCommand.java
@@ -19,7 +19,7 @@ public class RulesCommand implements Command {
     @Override
     public void execute(Update update, String args) {
         if (args == null || args.isEmpty()) {
-            sendMessageToThreadService.sendMessage(update.getMessage().getChatId().toString(), update.getMessage().getMessageThreadId(), RULES_MESSAGE.formatted("[NOMETA](https://nometa.xyz/ru.html)"),"MarkdownV2");
+            sendMessageToThreadService.sendMessage(update.getMessage().getChatId().toString(), update.getMessage().getMessageThreadId(), RULES_MESSAGE.formatted("[NOMETA](https://nometa.xyz/ru.html)"));
         }
     }
 }
